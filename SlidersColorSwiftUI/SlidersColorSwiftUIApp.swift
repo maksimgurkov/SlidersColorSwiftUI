2ee4762f52e5e6d719f576c44e108c7f2d83f871
Maksim Gurkov <maksim.gurkov@gmail.com>
2023-04-24 17:12:59 +0300

    Initial Commit

---
+++ SlidersColorSwiftUI/SlidersColorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersColorSwiftUIApp.swift
+//  SlidersColorSwiftUI
+//
+//  Created by Максим Гурков on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SlidersColorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
